a461d12417f302b42d8b24f3f154b9f6440ff669
John Marvin M. Ramos <johnmarvinramos18@gmail.com>
2025-07-05 12:51:28 +0800

    Update app.js

--- jiro/app.js
+++ jiro/app.js
@@ -1,21 +1,42 @@
-function openModal(img, poemText) {
-    let modal = document.getElementById('modal');
-    let modalImg = document.getElementById('modalImg');
-    let poem = document.getElementById('poem');
+const poemLines = [
+  "When I close my eyes, you're all I see",
+  "In dreams, your presence wraps around me.",
+  "Even when nightmares dare to intervene,",
+  "I find comfort in knowing you're in the scene.",
+  "You're my fear, my midnight fright,",
+  "Yet, in my mind, you dance in the moonlight.",
+  "I may not feel, I may not sense the real,",
+  "But love won't overshadow what dreams reveal.",
+  "Midnight Fright",
+];
 
-    modalImg.src = img.src;
-    poem.innerText = poemText;
+let poemIndex = -1;
 
-    modal.style.display = 'flex';
-    setTimeout(() => {
-        modal.classList.add('show');
-    }, 10);
+function openModal(img) {
+  let modal = document.getElementById("modal");
+  let modalImg = document.getElementById("modalImg");
+  let poem = document.getElementById("poem");
+
+  poem.innerText = poemLines[poemIndex];
+
+  poemIndex = (poemIndex + 1) % poemLines.length;
+
+  modalImg.src = img.src;
+  modal.style.display = "flex";
+
+  setTimeout(() => {
+    modal.classList.add("show");
+  }, 10);
 }
 
 function closeModal() {
-    let modal = document.getElementById('modal');
-    modal.classList.remove('show');
-    setTimeout(() => {
-        modal.style.display = 'none';
-    }, 500);
+  let modal = document.getElementById("modal");
+  modal.classList.remove("show");
+  setTimeout(() => {
+    modal.style.display = "none";
+  }, 500);
+}
+
+if (poemIndex < poemLines.length - 1) {
+  poemIndex++;
 }
